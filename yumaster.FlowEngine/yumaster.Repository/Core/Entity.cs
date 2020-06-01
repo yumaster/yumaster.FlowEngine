f5c09cb183866d62aba5886e3bf8ac4a8618e539
yumaster <yumaster@yeah.net>
2020-06-01 13:25:57 +0800

    add infrastructure

---
+++ yumaster.FlowEngine/yumaster.Repository/Core/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace yumaster.Repository.Core
+{
+    public abstract class Entity
+    {
+        [Browsable(false)]
+        public string Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
